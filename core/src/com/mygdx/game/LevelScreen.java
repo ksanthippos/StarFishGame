51376c966bf14c8cb833c41f47ea32def79c7d8e
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-05-16 19:07:54 +0300

    version 1.5 (updated baseactor)

--- core/src/com/mygdx/game/LevelScreen.java
+++ core/src/com/mygdx/game/LevelScreen.java
@@ -12,47 +12,27 @@ public class LevelScreen extends BaseScreen {
 
     private boolean win;
     private boolean lose;
-    private int collectedSF;
-
-    private List<Starfish> starfishList;
-    private List<Rock> rockList;
-    private List<Shark> sharkList;
 
     @Override
     public void initialize() {
+
         // background
         BaseActor ocean = new BaseActor(0, 0, mainStage);
         ocean.loadTexture("water-border.jpg");
         ocean.setSize(1200, 900);
         BaseActor.setWorldBounds(ocean);
 
-        // game objects
-        starfishList = new ArrayList<>();
-        rockList = new ArrayList<>();
-        sharkList = new ArrayList<>();
-
         // rocks & starfishes placed in fixed locations
-        Rock r1 = new Rock(300, 200, mainStage);
-        Rock r2 = new Rock(100, 300, mainStage);
-        Rock r3 = new Rock(400, 500, mainStage);
-        Rock r4 = new Rock(600, 700, mainStage);
+        new Rock(300, 200, mainStage);
+        new Rock(100, 300, mainStage);
+        new Rock(400, 500, mainStage);
+        new Rock(600, 700, mainStage);
 
-        rockList.add(r1);
-        rockList.add(r2);
-        rockList.add(r3);
-        rockList.add(r4);
-
-        Starfish s1 = new Starfish(200, 300, mainStage);
-        Starfish s2 = new Starfish(500, 800, mainStage);
-        Starfish s3 = new Starfish(900, 600, mainStage);
-        Starfish s4 = new Starfish(700, 100, mainStage);
-        Starfish s5 = new Starfish(300, 400, mainStage);
-
-        starfishList.add(s1);
-        starfishList.add(s2);
-        starfishList.add(s3);
-        starfishList.add(s4);
-        starfishList.add(s5);
+        new Starfish(200, 300, mainStage);
+        new Starfish(500, 800, mainStage);
+        new Starfish(900, 600, mainStage);
+        new Starfish(700, 100, mainStage);
+        new Starfish(300, 400, mainStage);
 
         // sharks placed randomly
         for (int i = 0; i < 5; i++) {
@@ -60,11 +40,10 @@ public class LevelScreen extends BaseScreen {
             int y = MathUtils.random(2, 10)*100;
             Shark shark = new Shark(x, y, mainStage);
             shark.setMaxSpeed(50);
-            sharkList.add(shark);
         }
 
         turtle = new Turtle(20, 20, mainStage);
-        collectedSF = 0;
+
         win = false;
         lose = false;
     }
@@ -72,8 +51,8 @@ public class LevelScreen extends BaseScreen {
     @Override
     public void update(float dt) {
 
-        for (Shark shark: sharkList) {
-            if (turtle.overlaps(shark) && !lose && !win) {
+        for (BaseActor sharkActor: BaseActor.getList(mainStage, Shark.class.getCanonicalName())) {
+            if (turtle.overlaps(sharkActor) && !lose && !win) {
                 lose = true;
                 turtle.remove();
                 BaseActor loseMessage = new BaseActor(0, 0, uiStage);
@@ -85,23 +64,25 @@ public class LevelScreen extends BaseScreen {
             }
         }
 
-        for (Rock rock: rockList)
-            turtle.preventOverlap(rock);
+        for (BaseActor rockActor: BaseActor.getList(mainStage, Rock.class.getCanonicalName()))
+            turtle.preventOverlap(rockActor);
 
-        for (Starfish starfish: starfishList) {
+        for (BaseActor starfishActor: BaseActor.getList(mainStage, Starfish.class.getCanonicalName())) {
 
-            if (turtle.overlaps(starfish) && !starfish.isCollected()) {
-                starfish.collect();
-                collectedSF++;
+            Starfish starfish = (Starfish) starfishActor;
+            if (turtle.overlaps(starfish) && !starfish.collected) {
+                starfish.collected = true;
                 starfish.clearActions();
                 starfish.addAction(Actions.fadeOut(1));
                 starfish.addAction(Actions.after(Actions.removeActor()));
+                starfishActor.remove();
+
                 Whirlpool whirlpool = new Whirlpool(0, 0, mainStage);
                 whirlpool.centerAtActor(starfish);
                 whirlpool.setOpacity(0.25f);
             }
 
-            if (collectedSF == starfishList.size() && !win) {
+            if (BaseActor.getList(mainStage, Starfish.class.getCanonicalName()).size() == 0 && !win) {
                 win = true;
                 BaseActor winMessage = new BaseActor(0, 0, uiStage);
                 winMessage.loadTexture("you-win.png");
